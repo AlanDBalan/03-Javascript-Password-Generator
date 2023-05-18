26a3040a0303776eb8fc904934685d5e4a1f18c4
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-05-18 10:52:48 -0500

    Update script.js
    Establishing a validator give parameters for generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+    // Initial variables and source code.
 var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
@@ -13,3 +14,7 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   var passwordLength = prompt("Please enter the desired password length (between 8 and 128 characters):");
   
+    // Validating length of password.
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Invalid length. Please enter a password length between 8 and 128 characters:");
+  }
